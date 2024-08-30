76c99996b242aff22f3d4297944bb9ac2431bfdd
Muhtasir Imran <mu.imran2002@gmail.com>
2024-08-30 17:57:47 +0600

    Added hypothesis 1 testing

--- hypothesis.py
+++ hypothesis.py
@@ -12,6 +12,7 @@ from interpretation_methods import beyond_intuition_tokenwise, beyond_intuition_
 from utils.model_loaders import vit_base_patch16_224_dino, vit_base_patch16_224
 from utils.imagenet_seg_loader import ImagenetSegLoader
 from utils.image_denorm import image_vizformat
+from torchvision import datasets
 from hyp.helpers import mismatched_image_corruption, find_mismatched_tokens
 from hyp.corrupt_data import get_corrupted_image
 from tqdm.auto import tqdm
@@ -45,9 +46,8 @@ class SaliencyGenerator(nn.Module):
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
         ])
-        self.label_transform = transforms.Compose([transforms.Resize((224, 224), Image.NEAREST), ])
 
-        self.dataset = ImagenetSegLoader(self.data_path, self.data_length, transform=self.image_transform, target_transform=self.label_transform)
+        self.dataset = datasets.ImageFolder(self.data_path, transform=self.image_transform)
         self.dataloader = DataLoader(self.dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, drop_last=False)
         self.dataloader = tqdm(self.dataloader)  # Would help tracking loop iteration along with setting some verbose text.
 
@@ -56,7 +56,7 @@ class SaliencyGenerator(nn.Module):
         self.model.eval()  # Set to eval mode. Helps tarcking gradient.
 
         self.saliency_writer = lambda output_path: torch.save(self.saliency_information, output_path)
-        self.accuracies = {}
+        self.accuracies = {'default': 0}
 
 
         print("""Please select a valid saliency method while calling the forward pass. Valid saliency methods are: 
@@ -68,43 +68,38 @@ class SaliencyGenerator(nn.Module):
     def find_accuracy(self, img, y):
         out = self.model(img)
         index = torch.argmax(out)
-        return index == y
+        return 1 if index == y else 0
 
 
     def make_heatmap(self, image, saliency):
-        if saliency == "ig":
-                heatmap = integrated_gradient(self.model, image, self.device)
+        if saliency == "ig": heatmap = integrated_gradient(self.model, image, self.device);
+        elif saliency == "bi_t": heatmap, _ = beyond_intuition_tokenwise(self.model, image, self.device, dino=True, start_layer=0);
+        elif saliency == "bi_h": heatmap = beyond_intuition_headwise(self.model, image, self.device, dino=True, start_layer=self.args.start_layer);
 
-        elif saliency == "bi_t":
-            heatmap, _ = beyond_intuition_tokenwise(self.model, image, self.device, dino=True, start_layer=0)
-            
-        elif saliency == "bi_h":
-            heatmap = beyond_intuition_headwise(self.model, image, self.device, dino=True, start_layer=self.args.start_layer)
-        
         return heatmap.reshape((14, 14)).detach().cpu()
         
 
     def forward(self, saliency="bi_t"):
-        for batch_idx, (image, labels) in enumerate(self.dataloader):
-            heatmaps = {}
-
+        for batch_idx, (image, target) in enumerate(self.dataloader):
             # Heatmap without any perturbation
-            r_ig_heatmap = self.make_heatmap(image, saliency="ig")
-            r_bit_heatmap = self.make_heatmap(image, saliency="bit")
-            heatmaps['raw_ig'] = r_ig_heatmap
-            heatmaps['raw_bit'] = r_bit_heatmap
+            rig_hmap, rbit_hmap = self.make_heatmap(image, saliency="ig"), self.make_heatmap(image, saliency="bit")
 
+            # Track accuracies
+            self.accuracies['default'] += self.find_accuracy(image, target) # Update default accuracy   
 
-
+            corrupted_stats = dict()
             for per in self.corrupt_percent:
-                corrupted_image = mismatched_image_corruption(image, r_ig_heatmap, r_bit_heatmap, corrupt_percentage=per)
-
+                corrupted_image = mismatched_image_corruption(image, rig_hmap, rbit_hmap, corrupt_percentage=per)
+                ig_hmp, bit_hmp = self.make_heatmap(image, saliency="ig"), self.make_heatmap(image, saliency="bit")
+                
+                corrupted_stats[f'per_{str(int(per * 100))}'] = {'heatmaps': [ig_hmp.tolist(), bit_hmp.tolist()], 
+                                'image': corrupted_image.tolist(), 'logit': self.model(image, register_hook=False).tolist()} # key = per_10 for 10%
 
 
             self.saliency_information[batch_idx] = {
-                'raw_ig': r_ig_heatmap.tolist(), 'raw_bit': r_bit_heatmap.tolist(),
-                'label': labels.tolist(),
-                'logit': self.model(image, register_hook=False).tolist()
+                'default': {'heatmap': [rig_hmap.tolist(), rbit_hmap.tolist()], 'target': target.tolist(), 
+                            'logit': self.model(image, register_hook=False).tolist(), 'image': image},
+                'corrupted': corrupted_stats,
             }
 
         self.saliency_writer(f'/content/benchmark/saliency_records/{saliency}_saliency.pt')
